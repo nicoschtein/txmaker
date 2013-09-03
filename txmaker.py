ee23e98ee36a4728d924cb8a1a14383ce9beb3be
Nicolas Schteinschraber <nicoschtein@gmail.com>
2013-09-03 19:20:51 -0300

    Added --lower, --upper and --multiplier to further customize the amount.

--- txmaker.py
+++ txmaker.py
@@ -21,6 +21,9 @@ group.add_argument("-c", "--count", help="the number of transactions to generate
 group.add_argument('-a', '--addresses', nargs='*', help='list of addresses to use, will repeat if less than count', default=['<address>'])
 parser.add_argument('-p', '--prefix', help='prefix for tx comment', default='backup')
 parser.add_argument("-f", "--fee", help="coind transaction fee", type=float, default='0.01')
+parser.add_argument("-l", "--lower", help="lower random number", type=float, default='1.0')
+parser.add_argument("-u", "--upper", help="upper random number", type=float, default='2.0')
+parser.add_argument("-m", "--multiplier", help="Multiplier for random", type=int, default='1000')
 parser.add_argument('-d', '--daemon', help='coin daemon name', default='coind')
 args = parser.parse_args()
 cantAdds = len(args.addresses)
@@ -38,7 +41,7 @@ print 'Transactions count: '+ str(L[0])
 print 'Aprox txfees: ' + str(fees)
 print 'Transactions:\n'
 for i in xrange(1,L[0]+1):
-  L.append(random.uniform(1.5,4.9)*1)
+  L.append(random.uniform(args.lower,args.upper)*args.multiplier)
   print args.daemon + ' sendtoaddress ' + args.addresses[i%cantAdds] + ' ' + '%.8f' % L[i] + ' \"' + args.prefix + ' ' + str(i) + '\"'
 total = sum(L[1:len(L)-1])
 print '\nTotal amount: ' + str(total)
